a51b2da1dcede90234a64ab6ca90593f3cd6924d
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2025-05-30 18:20:43 +0000

    Release v0.3.4

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'message_appcrue';
-$plugin->release = '0.3.3';
-$plugin->version = 2025050700;
+$plugin->release = '0.3.4';
+$plugin->version = 2025053000;
 $plugin->requires = 2015111600;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
