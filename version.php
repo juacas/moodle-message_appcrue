ec1c96bb37f856fc6848ba5b1bbd2d545c77bc2b
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2026-03-30 15:30:52 +0000

    Revert commit. Done in master.
    This reverts commit 7ca6c8782b38bc940170a5ab717867ac464a96b7.

--- version.php
+++ version.php
@@ -28,8 +28,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'message_appcrue';
-$plugin->release = '0.5.2';
-$plugin->version = 2026033000;
+$plugin->release = '0.5.1';
+$plugin->version = 2026011402;
 $plugin->requires = 2015111600;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
